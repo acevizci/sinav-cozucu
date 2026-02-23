028a29ebc927aab067523494b4390d587a151a8d
acevizci <118568517+acevizci@users.noreply.github.com>
2026-02-23 23:26:05 +0300

    Delete js directory

--- js/studio/math.js
+++
@@ -1,2 +0,0 @@
-export const clamp = (v,a,b) => Math.max(a, Math.min(b, v));
-export const snapPx = (v, grid=6) => Math.round(v / grid) * grid;
--- js/studio/state.js
+++
@@ -1,9 +0,0 @@
-export const State = {
-  pdfDoc: null,
-  img: null,
-  currentPage: 1,
-  scale: 1.0,
-  questions: [],
-  highlightQ: null,
-  undoStack: [],
-};
